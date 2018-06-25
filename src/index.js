67eea32d5ad50b4a22a3a43395b7ecb10ac1c6f8
xakoy <xakoy@qq.com>
2018-06-26 00:00:57 +0800

    webpack 加载图片

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import _ from 'lodash'
 import './style.css'
+import avatar from './avatar.gif';
 
 function component(){
     var element = document.createElement("div");
 
     element.innerHTML = _.join(['Hello', 'webpack'],' ')
     element.classList.add('hello');
-    
+
+    var myAvatar = new Image();
+    myAvatar.src = avatar;
+
+    element.appendChild(myAvatar);
+
     return element;
 }
 
